b1b986a82455fe2961b41490ce3cbd45e256602a
lemaicirabah <lemaicirabah@gmail.com>
2023-05-20 08:49:33 -0400

    ajout de la class FileReader

---
+++ calculetatcompte/src/calculetatcompte/FileReader.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package json;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+/**
+ *
+ * @author LP
+ */
+
+public class FileReader {
+    public static  String loadFileIntoString(String filePath, String fileEncoding) throws
+    FileNotFoundException, IOException {
+        return IOUtils.toString(new FileInputStream(filePath), fileEncoding);
+    }
+}
