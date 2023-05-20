8e983d97687eb0087d8c20c6ffe6ce92fef437e2
lemaicirabah <lemaicirabah@gmail.com>
2023-05-20 18:44:34 -0400

    Modification de la class GestionEmploye ajout de la methode RecupererJson

--- src/calculetatcomptes/GestionEmploye.java
+++ src/calculetatcomptes/GestionEmploye.java
@@ -6,12 +6,41 @@ package calculetatcomptes;
 import java.io.IOException;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
-
 /**
  *
  * @author LP
  */
-public class GestionEmploye extends Employe{
+public class GestionEmploye {
     
+    public static final String MATRICULE_EMPLOYE="matricule_employe";
+    public static final String TYPE_EMPLOYE="type_employe";
+    public static final String TAUX_HORAIRES_MIN= "taux_horaire_min";
+    public static final String TAUX_HORAIRES_MAX="taux_horaire_max";
+    public static final String INTERVENTION= "interventions";      
+    public static final String CODE_CLIENT="code_client";
+    public static final String DISTANCE_DEPLACEMENT="distance_deplacement";
+    public static final String OVERTIME="overtime";
+    public static final String NOMBRES_HEURES="nombre_heures";
+    public static final String DATE_INTERVENTION="date_intervention";        
+    
+            
+    public  Employe RecupererJson() throws IOException {
+    
+        Employe objetEmploye=null;
+        String myJSON = FileReader.loadFileIntoString("Json.txt",
+       "UTF-8");
+        JSONObject employe=JSONObject.fromObject(myJSON);
+        int matriculeLocal= employe.getInt(MATRICULE_EMPLOYE);
+        objetEmploye.setMatricule(matriculeLocal);
+        int typeEmployeLocal= employe.getInt(TYPE_EMPLOYE);
+        objetEmploye.setTauxMin(typeEmployeLocal);
+        int tauxMinLocal= employe.getInt(TAUX_HORAIRES_MIN);
+        objetEmploye.setTauxMin(tauxMinLocal);
+        int tauxMaxLocal= employe.getInt(TAUX_HORAIRES_MAX);
+        objetEmploye.setTauxMax(tauxMaxLocal);
+        
+        
+        return objetEmploye;
+    }
     
 }
