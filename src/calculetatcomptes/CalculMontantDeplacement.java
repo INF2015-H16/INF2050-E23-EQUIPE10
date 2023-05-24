bc49562e8056d77a32debe9e874cd6b67435ca7d
lesuareg <ed091952@ens.uqam.ca>
2023-05-23 22:04:55 -0400

    Modification class CalculMontantDeplacement

--- src/calculetatcomptes/CalculMontantDeplacement.java
+++ src/calculetatcomptes/CalculMontantDeplacement.java
@@ -5,65 +5,42 @@
  */
 package calculetatcomptes;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 /**
  *
  * @author lesuareg
  */
+
 public class CalculMontantDeplacement {
+
+    private String codeClient;
+    private double montantDeplacement;
+    
+    
+    public String getCodeClient() {
+        return codeClient;
+    }
+
+    public void setCodeClient(String codeClient) {
+        this.codeClient = codeClient;
+    }
+
+    public double getMontantDeplacement() {
+        return montantDeplacement;
+    }
+
+    public void setMontantDeplacement(double montantDeplacement) {
+        this.montantDeplacement = montantDeplacement;
+    }
 
     /**
      *
-     * @param employes
+     * @param nmbr
      * @return
      */
-    public static JSONArray calcul(JSONArray employes) {
-
-        JSONArray interventions = new JSONArray();
-        JSONArray montant = new JSONArray();
-        JSONArray etatClient = new JSONArray();
-
-        JSONObject unMontant = new JSONObject();
-        JSONObject unEtatClient = new JSONObject();
-
-        JSONObject unEmploye, uneIntervention, uneAutreIntervention;
-
-        for (int i = 0; i < employes.size(); i++) {
-            unEmploye = employes.getJSONObject(i);
-
-            if (unEmploye.getInt("type_employe") == 0) {
-                unEtatClient.put("matricule_employe", unEmploye.getString("matricule_employe"));
-                interventions = unEmploye.getJSONArray("interventions");
-                double cout = 0;
-                for (int j = 0; j < interventions.size() - 1; j++) {
-                    uneIntervention = interventions.getJSONObject(j);
-
-                    for (int k = j + 1; k < interventions.size(); k++) {
-                        uneAutreIntervention = interventions.getJSONObject(k);
-
-                        if (uneIntervention.getString("code_client").equalsIgnoreCase(uneAutreIntervention.getString("code_client"))) {
-                            cout += 200.00 - ((uneIntervention.getInt("distance_deplacement") + uneAutreIntervention.getInt("distance_deplacement")*(5/100)));//*montant regulier   
-                            unMontant.put("code_client", uneIntervention.getString("code_client"));
-                            unMontant.put("etat_par_client", cout);
-                            montant.add(unMontant);
-                            cout = 0;
-                        }
-                    }
-                    if (!uneIntervention.getString("code_client").equalsIgnoreCase(unMontant.getString("code_client"))) {
-                        cout += 200.00 - (uneIntervention.getInt("distance_deplacement")*(5/100)); //*montant regulier                     
-                        unMontant.put("code_client", uneIntervention.getString("code_client"));
-                        unMontant.put("etat_par_client", cout);
-                        montant.add(unMontant);
-                        cout = 0;
-                    }
-                }
-                unEtatClient.put("clients", montant);
-                etatClient.add(unEtatClient);
-                montant.clear();
-            }
-        }
-        return etatClient;
+    public static void calculMontanDeplacement() {
+       
+        
     }
 }
+
+    
