79020f79acff2f2da41f714abe0607d314e374c2
seif <seifsaidi1993@gmail.com>
2023-05-27 01:36:19 +0100

    Ajout de la methode verifCodesClient qui verifie si le code d'un client est répété pour ne pas faire le calcul d'un même clien plusieur fois

--- src/calculetatcomptes/CalculMontantRegulier.java
+++ src/calculetatcomptes/CalculMontantRegulier.java
@@ -18,6 +18,16 @@ public class CalculMontantRegulier {
         chaineConvertit = Double.parseDouble(chaine);
         return chaineConvertit;
     }
+    private static boolean verifCodesClient(String codeClient, String tab[]) {
+        boolean verif = false;
+        for (String tab1 : tab) {
+            if (codeClient.equals(tab1)) {
+                verif = true;
+                break;
+            }
+        }
+        return verif;
+    }
 
     
 }
