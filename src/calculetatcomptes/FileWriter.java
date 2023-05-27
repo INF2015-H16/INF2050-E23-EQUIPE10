3891fe97c7765c573df27ff6f20f7d842a1071c4
lemaicirabah <lemaicirabah@gmail.com>
2023-05-26 23:51:30 -0400

    j'ai creer la class FileWriter  pour ecrire dans le fichier Json

---
+++ src/calculetatcomptes/FileWriter.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculetatcomptes;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author rabahlemaici
+ */
+public class FileWriter {
+    
+    public static void saveStringIntoFile(String filePath, String contentToSave)
+    throws FileNotFoundException, IOException {
+        File f = new File(filePath);
+        FileUtils.writeStringToFile(f, contentToSave, "UTF-8");
+    } 
+}
