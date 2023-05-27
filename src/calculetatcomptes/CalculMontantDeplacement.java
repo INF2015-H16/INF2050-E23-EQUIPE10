9b61799d509c7be0e0c14b0681cbd7ca9d6719dc
lesuareg <ed091952@ens.uqam.ca>
2023-05-27 01:42:04 -0400

    Ajout des methodes pour calculer le montant pour deplacement

--- src/calculetatcomptes/CalculMontantDeplacement.java
+++ src/calculetatcomptes/CalculMontantDeplacement.java
@@ -5,17 +5,24 @@
  */
 package calculetatcomptes;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author lesuareg
  */
-
 public class CalculMontantDeplacement {
 
     private String codeClient;
     private double montantDeplacement;
-    
-    
+
+    public CalculMontantDeplacement(String codeClient, double montantDeplacement) {
+        this.codeClient = codeClient;
+        this.montantDeplacement = montantDeplacement;
+    }
+
     public String getCodeClient() {
         return codeClient;
     }
@@ -32,15 +39,64 @@ public class CalculMontantDeplacement {
         this.montantDeplacement = montantDeplacement;
     }
 
+    private static HashMap<String, Integer> listeClientsUnique(ArrayList<Intervention> interventions) {
+
+        String codeClient;
+        Integer distance;
+        HashMap<String, Integer> listeUnique = new HashMap<String, Integer>();
+
+        for (Intervention i : interventions) {
+            codeClient = i.getCodeClient();
+            distance = i.getDistanceDeplacement();
+            if (listeUnique.containsKey(codeClient)) {
+                distance += listeUnique.get(i);
+                listeUnique.put(codeClient, distance);
+            } else {
+                listeUnique.put(codeClient, distance);
+            }
+        }
+        return listeUnique;
+    }
+
+    private static void calculMontantType0(ArrayList<Intervention> interventions) {
+        HashMap<String, Integer> listeUnique;
+        listeUnique = listeClientsUnique(interventions);
+    }
+
+    private static void calculMontantType1(ArrayList<Intervention> interventions) {
+        HashMap<String, Integer> listeUnique;
+        listeUnique = listeClientsUnique(interventions);
+    }
+
+    private static void calculMontantType2(ArrayList<Intervention> interventions) {
+        HashMap<String, Integer> listeUnique;
+        listeUnique = listeClientsUnique(interventions);
+    }
+
     /**
      *
-     * @param nmbr
-     * @return
+     * @throws java.io.IOException
      */
-    public static void calculMontanDeplacement() {
-       
-        
+    public static void calculMontanDeplacement() throws IOException {
+
+        Employe employe;
+        employe = GestionEmploye.RecupererJson();
+
+        ArrayList<Intervention> interventions;
+        interventions = employe.getInterventions();
+
+        switch (employe.getTypeEmploye()) {
+            case 0:
+                calculMontantType0(interventions);
+                break;
+            case 1:
+                calculMontantType1(interventions);
+                break;
+            case 2:
+                calculMontantType2(interventions);
+                break;
+            default:
+                break;
+        }
     }
 }
-
-    
