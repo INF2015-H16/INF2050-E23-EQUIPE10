b1951ce2e4c5d4da4bc8a6ac1373d67b5df47f54
lesuareg <ed091952@ens.uqam.ca>
2023-05-27 18:11:02 -0400

    Ajout methodes calculMontantTotal et calculMontantType

--- src/calculetatcomptes/CalculMontantDeplacement.java
+++ src/calculetatcomptes/CalculMontantDeplacement.java
@@ -7,9 +7,12 @@ package calculetatcomptes;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -41,47 +44,90 @@ public class CalculMontantDeplacement {
         this.montantDeplacement = montantDeplacement;
     }
 
-    private static HashMap<String, Integer> listeClientsUnique(ArrayList<Intervention> interventions) {
-
+    private static ArrayList<Entry<String, Integer>> listeClientsUnique(ArrayList<Intervention> interventions) {
         String codeClient;
         Integer distance;
-        HashMap<String, Integer> listeUnique = new HashMap<String, Integer>();
+        HashMap<String, Integer> mapUnique = new HashMap<>();
 
         for (Intervention i : interventions) {
             codeClient = i.getCodeClient();
             distance = i.getDistanceDeplacement();
-            if (listeUnique.containsKey(codeClient)) {
-                distance += listeUnique.get(i);
-                listeUnique.put(codeClient, distance);
+            if (mapUnique.containsKey(codeClient)) {
+                distance += mapUnique.get(codeClient);
+                mapUnique.put(codeClient, distance);
             } else {
-                listeUnique.put(codeClient, distance);
+                mapUnique.put(codeClient, distance);
             }
         }
+
+        List<String> uniqueList = mapUnique.keySet().stream().collect(Collectors.toList());
+        Set<Map.Entry<String, Integer>> entrySet
+                = mapUnique.entrySet();
+        ArrayList<Map.Entry<String, Integer>> listeUnique;
+        listeUnique = new ArrayList<>(entrySet);
+
         return listeUnique;
     }
 
-    private static CalculMontantDeplacement[] calculMontantType0(HashMap<String, Integer> listeUnique) throws IOException {
+    private static double calculMontantTotal(double montantRegulier, double montantDeplacement1, int par) {
+        double montantTotal = 0;
+        switch (par) {
+            case 0:
+                montantTotal = 200.0 - (montantDeplacement1 * (5 / 100.0) * montantRegulier);
+                break;
+            case 1:
+                montantTotal = 200.0 - (montantDeplacement1 * (10 / 100.0) * montantRegulier);
+                break;
+            case 2:
+                montantTotal = 200.0 - (montantDeplacement1 * (15 / 100.0) * montantRegulier);
+                break;
+            default:
+                break;
+        }
+        return montantTotal;
+    }
 
+    private static CalculMontantDeplacement[] calculMontantType(ArrayList<Entry<String, Integer>> listeUnique, int par) throws IOException {
+        double montantTotal, montantA, montantB;
+        String codeA, codeB;
         
-        CalculMontantDeplacement[] montant = null;
-        
+        CalculMontantDeplacement[] deplacement = new CalculMontantDeplacement[listeUnique.size()];
+        CalculMontantDeplacement[] montant = new CalculMontantDeplacement[listeUnique.size()];
+
+        ObjetMontantRegulier[] montantReguliere;
+        montantReguliere = CalculMontantRegulier.calculMontant();
+        /*
+        ObjetMontantRegulier[] montantReguliere = {new ObjetMontantRegulier("C123", 1000), //montant reguliere pour test
+            new ObjetMontantRegulier("C456", 1000), new ObjetMontantRegulier("C789", 1000)};
+        */
+        int compteur = 0;
+
+        for (Entry<String, Integer> b : listeUnique) {
+            deplacement[compteur] = new CalculMontantDeplacement(b.getKey(), b.getValue());
+            compteur++;
+        }
+
+        for (int i = 0; i < montantReguliere.length; i++) {
+            for (CalculMontantDeplacement montantDepl1 : deplacement) {
+                codeA = montantReguliere[i].getCodeClient();
+                codeB = montantDepl1.getCodeClient();
+                montantA = montantReguliere[i].getMontantRegulier();
+                montantB = montantDepl1.getMontantDeplacement();
+                if (codeA.equalsIgnoreCase(codeB)) {
+                    montantTotal = calculMontantTotal(montantA, montantB, par);
+                    montant[i] = new CalculMontantDeplacement(codeA, montantTotal);
+                }
+            }
+        }
 
         return montant;
     }
 
-    private static void calculMontantType1(ArrayList<Intervention> interventions) {
-        
-    }
-
-    private static void calculMontantType2(ArrayList<Intervention> interventions) {
-       
-    }
-
     /**
      *
-     * @throws java.io.IOException
+     * @return @throws java.io.IOException
      */
-    public static void calculMontanDeplacement() throws IOException {
+    public static CalculMontantDeplacement[] calculMontanDeplacement() throws IOException {
 
         Employe employe;
         employe = GestionEmploye.RecupererJson();
@@ -89,22 +135,12 @@ public class CalculMontantDeplacement {
         ArrayList<Intervention> interventions;
         interventions = employe.getInterventions();
 
-        HashMap<String, Integer> listeUnique;
+        ArrayList<Entry<String, Integer>> listeUnique;
         listeUnique = listeClientsUnique(interventions);
 
-        switch (employe.getTypeEmploye()) {
-            case 0:
-                CalculMontantDeplacement[] calculMontantType0 = calculMontantType0(listeUnique); //
-                break;
+        CalculMontantDeplacement[] calculMontant;
+        calculMontant = calculMontantType(listeUnique, employe.getTypeEmploye());
 
-            case 1:
-                // calculMontantType1(interventions);
-                break;
-            case 2:
-                //calculMontantType2(interventions);
-                break;
-            default:
-                break;
-        }
+        return calculMontant;
     }
 }
