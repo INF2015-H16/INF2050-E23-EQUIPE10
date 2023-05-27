f29204fc41afa1b9b695af8ed682b62b61693da7
seif <seifsaidi1993@gmail.com>
2023-05-27 01:28:46 +0100

    gestion de la class ObjetMontantRegulier : ajout les methodes getCodeClient, getMontantRegulier, setCodeClient, setMontantRegulier

--- src/calculetatcomptes/ObjetMontantRegulier.java
+++ src/calculetatcomptes/ObjetMontantRegulier.java
@@ -11,6 +11,32 @@ package calculetatcomptes;
  */
 public class ObjetMontantRegulier {
    
+    private String codeClient;
+    private double montantRegulier;
+       
+
+    
+    public ObjetMontantRegulier( String codeClient,double montantRegulier){
+        this.codeClient=codeClient;
+        this.montantRegulier=montantRegulier;
+    
+    }
+    
+    public String getCodeClient() {
+        return codeClient;
+    }
+    
+    public double getMontantRegulier() {
+        return montantRegulier;
+    }
+
+    public void setCodeClient(String codeClient) {
+        this.codeClient = codeClient;
+    }
+    
+    public void setMontantRegulier(double montantRegulier) {
+        this.montantRegulier = montantRegulier;
+    }
 
    
     
