daca656979dcf2bf25cbc9521485e9474f063acb
lesuareg <ed091952@ens.uqam.ca>
2023-07-02 00:24:20 -0400

    Modification 8 lignes de codes par méthode

--- src/calculetatcomptes/CalculerMontantDeplacement.java
+++ src/calculetatcomptes/CalculerMontantDeplacement.java
@@ -5,19 +5,17 @@
  */
 package calculetatcomptes;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
- * Class CalculerMontantDeplacement:
- Description: classe qui sert à calculer le montant pour le déplacement 
-              des interventions pour les clients selon le type d'employé
+ * Class CalculerMontantDeplacement: Description: classe qui sert à calculer le
+ * montant pour le déplacement des interventions pour les clients selon le type
+ * d'employé
+ *
  * @author Luz Enith Suarez Gonzalez
  */
 public class CalculerMontantDeplacement {
@@ -25,22 +23,19 @@ public class CalculerMontantDeplacement {
     private String codeClient;
     private double montantDeplacement;
 
-  
     public CalculerMontantDeplacement(String codeClient, double montantDeplacement) {
         this.codeClient = codeClient;
         this.montantDeplacement = montantDeplacement;
     }
 
-
     public String getCodeClient() {
         return codeClient;
     }
-    
+
     public double getMontantDeplacement() {
         return montantDeplacement;
     }
-    
-  
+
     public void setCodeClient(String codeClient) {
         this.codeClient = codeClient;
     }
@@ -49,127 +44,13 @@ public class CalculerMontantDeplacement {
         this.montantDeplacement = montantDeplacement;
     }
 
-    // Methode listeClientsUnique:
-    // Description: cree un map avec la liste de clients et la distance de deplacement,
-    //              si le code de client se repete, il ajoute cette distance de 
-    //              deplacement à la distance de deplacement qui est déjà dans le map. 
-    //              Le map est converti dans un array list qui contien le code du client 
-    //              et la distance de deplacement total pour ce client
-    // RETOUR: ArrayList<Entry<String, Integer>>, liste des codes du client et leur distance 
-    //     total de deplacement
-    // IN: 
-    //     ArrayList<Intervention> interventions : liste des interventions 
-    private static ArrayList<Entry<String, Integer>> listeClientsUnique(ArrayList<Intervention> interventions) {
-        String codeClient;
-        Integer distance;
-        HashMap<String, Integer> mapUnique = new HashMap<>();
-
-        for (Intervention i : interventions) {
-            codeClient = i.getCodeClient();
-            distance = i.getDistanceDeplacement();
-            if (mapUnique.containsKey(codeClient)) {
-                distance += mapUnique.get(codeClient);
-                mapUnique.put(codeClient, distance);
-            } else {
-                mapUnique.put(codeClient, distance);
-            }
-        }
-
-        List<String> uniqueList = mapUnique.keySet().stream().collect(Collectors.toList());
-        Set<Map.Entry<String, Integer>> entrySet
-                = mapUnique.entrySet();
-        ArrayList<Map.Entry<String, Integer>> listeUnique;
-        listeUnique = new ArrayList<>(entrySet);
-
-        return listeUnique;
-    }
-
-    // Methode calculerMontantTotal:
-    // Description: calcule le montant pour le déplacement du client selon le type d'émploye
-    // RETOUR: double, montant total pour le déplacement de chaque client 
-    // IN: 
-    //     double montantRegulier : montant régulier du client
-    //     double distanceDeplacement : distance de déplacement du client
-    //     int type : type d'employé
-    private static double calculerMontantTotal(double montantRegulier, double distanceDeplacement, int type) {
-        double montantTotal = 0;
-        switch (type) {
-            case 0:
-                montantTotal = 200.0 - (distanceDeplacement * (5 / 100.0) * montantRegulier);
-                break;
-            case 1:
-                montantTotal = 200.0 - (distanceDeplacement * (10 / 100.0) * montantRegulier);
-                break;
-            case 2:
-                montantTotal = 200.0 - (distanceDeplacement * (15 / 100.0) * montantRegulier);
-                break;
-            default:
-                break;
-        }
-        return montantTotal;
-    }
-     
-    // Methode calculerMontantType:
-    // Description: crée un tableau avec le code du client et leur distance total de 
-    //              déplacement à partir de la liste unique
-    //              il parcourt les deux tableaux: montantReguliere (avec le montant régulière)  
-    //              et deplacementClient (avec la distance de déplacement), s'ils ont le
-    //              même code du client il calcule le montant pour déplacement et l'ajoute dans 
-    //              le tableaux le montant avec le respective code du client
-    // RETOUR: CalculerMontantDeplacement[], tableau avec les codes du client et leur montant pour
-    //         le déplacement
-    // IN: 
-    //     ArrayList<Entry<String, Integer>> listeUnique : liste des clients et leur distance 
-    //                                                     total de deplacement
-    //     int type : type d'employé
-    private static CalculerMontantDeplacement[] calculerMontantType(ArrayList<Entry<String, Integer>> listeUnique, int type) throws Exception, ClassExceptions  {
-        double montantTotal, montantReg, montantDep;
-        String codeClMonReg, codeClMonDep;
-        
-        CalculerMontantDeplacement[] deplacementClient = new CalculerMontantDeplacement[listeUnique.size()];
-        CalculerMontantDeplacement[] montant = new CalculerMontantDeplacement[listeUnique.size()];
-
-        ObjetMontantRegulier[] montantReguliere;
-        montantReguliere = CalculMontantRegulier.calculMontant();
-      
-        int compteur = 0;
-
-        for (Entry<String, Integer> b : listeUnique) {
-            deplacementClient[compteur] = new CalculerMontantDeplacement(b.getKey(), b.getValue());
-            compteur++;
-        }
-
-        for (int i = 0; i < montantReguliere.length; i++) {
-            for (CalculerMontantDeplacement montantDepl : deplacementClient) {
-                codeClMonReg = montantReguliere[i].getCodeClient();
-                codeClMonDep = montantDepl.getCodeClient();
-                montantReg = montantReguliere[i].getMontantRegulier();
-                montantDep = montantDepl.getMontantDeplacement();
-                if (codeClMonReg.equalsIgnoreCase(codeClMonDep)) {
-                    montantTotal = calculerMontantTotal(montantReg, montantDep, type);
-                    montant[i] = new CalculerMontantDeplacement(codeClMonReg, montantTotal);
-                }
-            }
-        }
-        return montant;
-    }
- 
-    // Methode calculerMontanDeplacement:
-    // Description: recupere un employé et la liste des interventions du fichier JSON
-    //              recupere la liste unique des codes du client et leur distance total de deplacement     
-    //              calcule le montant pour déplacement pour chaque code du client selon le 
-    //              type d'émploye
-    // RETOUR: CalculerMontantDeplacement[], tableau avec les codes du client et leur montant pour
-    //         le déplacement
-     /**
-     * @return 
-     * @throws java.io.IOException
+    /**
+     * @return @throws java.lang.Exception
      * @throws calculetatcomptes.ClassExceptions
      */
-    public static CalculerMontantDeplacement[] calculerMontanDeplacement() throws Exception, ClassExceptions  {
+    public static CalculerMontantDeplacement[] calculerMontanDeplacement() throws Exception, ClassExceptions {
 
-        Employe employe;
-        employe = GestionEmploye.creerEmployeFromJson();
+        Employe employe = GestionEmploye.creerEmployeFromJson();
 
         ArrayList<Intervention> interventions;
         interventions = employe.getInterventions();
@@ -181,5 +62,103 @@ public class CalculerMontantDeplacement {
         calculMontant = calculerMontantType(listeUnique, employe.getTypeEmploye());
 
         return calculMontant;
+    }
+
+    /**
+     * @return
+     */
+    private static ArrayList<Entry<String, Integer>> listeClientsUnique(ArrayList<Intervention> interventions) {
+
+        HashMap<String, Integer> mapUnique = creerMapClientsUnique(interventions);
+
+        Set<Map.Entry<String, Integer>> entrySet
+                = mapUnique.entrySet();
+
+        ArrayList<Map.Entry<String, Integer>> listeUnique;
+        listeUnique = new ArrayList<>(entrySet);
+
+        return listeUnique;
+    }
+
+    /**
+     * @return
+     */
+    private static HashMap<String, Integer> creerMapClientsUnique(ArrayList<Intervention> interventions) {
+
+        HashMap<String, Integer> mapUnique = new HashMap<>();
+
+        interventions.forEach(intervention -> {
+            Integer distance = intervention.getDistanceDeplacement();
+            if (mapUnique.containsKey(intervention.getCodeClient())) {
+                distance += mapUnique.get(intervention.getCodeClient());
+                mapUnique.put(intervention.getCodeClient(), distance);
+            } else {
+                mapUnique.put(intervention.getCodeClient(), distance);
+            }
+        });
+
+        return mapUnique;
+    }
+
+    /**
+     * @return
+     */
+    private static CalculerMontantDeplacement[] calculerMontantType(ArrayList<Entry<String, Integer>> listeUnique, int type) throws Exception, ClassExceptions {
+
+        CalculerMontantDeplacement[] deplacementClient = new CalculerMontantDeplacement[listeUnique.size()];
+        ObjetMontantRegulier[] montantReguliere = CalculMontantRegulier.calculMontant();
+
+        int compteur = 0;
+
+        for (Entry<String, Integer> client : listeUnique) {
+            deplacementClient[compteur] = new CalculerMontantDeplacement(client.getKey(), client.getValue());
+            compteur++;
+        }
+
+        CalculerMontantDeplacement[] montant = calculerMontantListeUnique(listeUnique, montantReguliere, deplacementClient, type);
+
+        return montant;
+    }
+
+    /**
+     * @return
+     */
+    private static CalculerMontantDeplacement[] calculerMontantListeUnique(ArrayList<Entry<String, Integer>> listeUnique, ObjetMontantRegulier[] montantReguliere, CalculerMontantDeplacement[] deplacementClient, int type) {
+
+        CalculerMontantDeplacement[] montant = new CalculerMontantDeplacement[listeUnique.size()];
+
+        for (int i = 0; i < montantReguliere.length; i++) {
+
+            for (CalculerMontantDeplacement montantDepl : deplacementClient) {
+
+                if (montantReguliere[i].getCodeClient().equalsIgnoreCase(montantDepl.getCodeClient())) {
+                    double montantTotal = calculerMontantTotal(montantReguliere[i].getMontantRegulier(),
+                            montantDepl.getMontantDeplacement(), type);
+                    montant[i] = new CalculerMontantDeplacement(montantReguliere[i].getCodeClient(), montantTotal);
+                }
+            }
+        }
+        return montant;
+    }
+
+     /**
+     * @return
+     */
+    private static double calculerMontantTotal(double montantRegulier, double distanceDeplacement, int type) {
+        double montantTotal = 0;
+        switch (type) {
+            case 0:
+                montantTotal = 200.0 - (distanceDeplacement * (5 / 100.0) * montantRegulier); //5%
+                break;
+            case 1:
+                montantTotal = 200.0 - (distanceDeplacement * (10 / 100.0) * montantRegulier); //10%
+                break;
+            case 2:
+                montantTotal = 200.0 - (distanceDeplacement * (15 / 100.0) * montantRegulier); //15%
+                break;
+            default:
+                break;
+        }
+        return montantTotal;
     }
 }
