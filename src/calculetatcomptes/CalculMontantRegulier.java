97facb615da0c14543cc61dee7ab8f31954a8240
Aymen Messaad <bg991952@ens.uqam.ca>
2023-07-03 15:40:53 -0400

    teste de la classe CalculMontantRegulier (fonctionne bien)

--- src/calculetatcomptes/CalculMontantRegulier.java
+++ src/calculetatcomptes/CalculMontantRegulier.java
@@ -8,16 +8,10 @@ package calculetatcomptes;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Cette class permet de calculer le montant régulier de chaque client
- *
- * @author seif saidi
- */
+
 public class CalculMontantRegulier {
 
-    /**
-     * @return @throws calculetatcomptes.ClassExceptions
-     */
+    
     public static ObjetMontantRegulier[] calculMontant() throws Exception, ClassExceptions {
 
         Employe employe = GestionEmploye.creerEmployeFromJson();
@@ -35,13 +29,7 @@ public class CalculMontantRegulier {
         return tabObjetFinal;
     }
 
-    /**
-     * @param employe
-     * @param tabCodesClient
-     * @param interventions
-     * @return
-     * @throws java.io.IOException
-     */
+    
     private static ObjetMontantRegulier[] creerTableauObjetFinal(Employe employe,
             String tabCodesClient[], ArrayList<Intervention> interventions) throws Exception {
 
@@ -57,16 +45,7 @@ public class CalculMontantRegulier {
         return tabObjetFinal;
     }
 
-    /**
-     * @param tabCodesClient
-     * @param interventions
-     * @param typeEmploye
-     * @param tauxHoraireMin
-     * @param tauxHoraireMax
-     * @param tauxHoraireMoyen
-     * @return
-     * @throws java.io.IOException
-     */
+    
     private static ObjetMontantRegulier[] creerTableauObjetFinalType(String tabCodesClient[],
             ArrayList<Intervention> interventions, int typeEmploye, double tauxHoraireMin,
             double tauxHoraireMax, double tauxHoraireMoyen) throws IOException {
@@ -87,13 +66,7 @@ public class CalculMontantRegulier {
         return tabObjetFinal;
     }
 
-    /**
-     * @param tabCodesClient
-     * @param interventions
-     * @param tauxHoraire
-     * @return
-     * @throws java.io.IOException
-     */
+    
     private static ObjetMontantRegulier[] calculMontantReguliere(String tabCodesClient[],
             ArrayList<Intervention> interventions, double tauxHoraire) throws IOException {
 
@@ -116,15 +89,7 @@ public class CalculMontantRegulier {
         return tabObjetFinal;
     }
 
-    /**
-     * @param interventions
-     * @param tabCodesClient
-     * @param i
-     * @param nbHeure
-     * @param tauxHoraire
-     * @return
-     * @throws java.io.IOException
-     */
+    
     private static double montantReguliere(ArrayList<Intervention> interventions,
             String[] tabCodesClient, int i, int nbHeure, double tauxHoraire) {
 
@@ -139,13 +104,7 @@ public class CalculMontantRegulier {
         return montantRegulier;
     }
 
-    /**
-     * @param tabPourVérifier
-     * @param i
-     * @param tabCodesClient
-     * @return
-     * @throws java.io.IOException
-     */
+    
     private static boolean isNouveauCodeClient(String[] tabCodesClient, String[] tabPourVérifier, int i) throws IOException {
 
         boolean nouveauCodeClient = false;
@@ -158,12 +117,7 @@ public class CalculMontantRegulier {
         return nouveauCodeClient;
     }
 
-    /**
-     * @param codeClient
-     * @param tabCodesClient
-     * @return
-     * @throws java.io.IOException
-     */
+    
     private static boolean verifierCodesClient(String codeClient, String[] tabCodesClient) throws IOException {
 
         boolean verifier = false;
@@ -177,12 +131,8 @@ public class CalculMontantRegulier {
 
         return verifier;
     }
-
-    /**
-     * @param tabObjet
-     * @param compteur
-     * @return
-     */
+    
+    
     private static ObjetMontantRegulier[] creerTableauObjet(ObjetMontantRegulier tabObjet[], int compteur) {
 
         ObjetMontantRegulier tabObjetFinal[] = new ObjetMontantRegulier[compteur];
